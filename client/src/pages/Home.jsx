9f3dc8ff2da948997bfe4b5b787bd2fa4f4d33f0
Dhiraj Kumar <dhirajkrraj824113@gmail.com>
2025-11-24 14:05:22 +0530

    Initialized repo

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeatureSection from '../components/FeatureSection'
+import MovieCard from '../components/MovieCard'
+import TrailerSection from '../components/TrailerSection'
+
+const Home = () => {
+  return (
+
+  
+  <>
+  <HeroSection/>
+  <FeatureSection/>
+  <TrailerSection/>
+  </>
+  )
+}
+
+export default Home
---
+++ client/src/pages/MyBooking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SeatLayout from './SeatLayout'
+
+const MyBooking = () => {
+  return (
+    <div></div>
+ 
+  )
+}
+
+export default MyBooking
