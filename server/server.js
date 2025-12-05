29b2a9a92917697360aab8182186a17f65e34420
Dhiraj Kumar <dhirajkrraj824113@gmail.com>
2025-12-05 11:54:02 +0530

    update the package.json and package-lock.json.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
@@ -12,6 +13,8 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(clerkMiddleware());
 
 //API routes
 app.get("/", (req, res) => res.send("Server is Live!"));
